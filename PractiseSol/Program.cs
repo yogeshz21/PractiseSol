c57cae7e225dc2cf3a5e6d29a5fe4207c6bf1bc1
Yogesh Zagade <yogesh.zagade21@gmail.com>
2021-06-12 11:57:59 +0530

    Added few changes

--- PractiseSol/Program.cs
+++ PractiseSol/Program.cs
@@ -6,7 +6,44 @@ namespace PractiseSol
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            A s = new Sample();
+            s.TestMethod(2,3);
+            Console.ReadKey();
+        }
+
+        abstract class A
+        {
+            public virtual void TestMethod(int i, int j)
+            {
+                Console.WriteLine("class A => TestMethod()");
+            }
+
+            public virtual void TestMethod(int i, int j, int k)
+            {
+                Console.WriteLine("class A => overload TestMethod()");               
+            }
+            public abstract void TestMethodB();
+        }
+
+        abstract class B
+        {
+            public void TestMethod(int i, int j)
+            {
+                Console.WriteLine("class B => TestMethod()");
+            }
+        }
+
+        class Sample : A
+        {
+            public override void TestMethod(int a, int b)
+            {
+                Console.WriteLine("class SAMPLE => TestMethod()");
+            }
+
+            public override void TestMethodB()
+            {
+                Console.WriteLine("class SAMPLE => TestMethodB()");
+            }
         }
     }
 }
